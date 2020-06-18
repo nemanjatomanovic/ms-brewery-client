eb018a92147dba3059b77fc17f579ea47d867bbe
Nemanja Tomanović <nemanja.tomanovic@gmail.com>
2020-06-18 06:48:46 +0200

    configuring blocking restTemplate

---
+++ src/main/java/com/brewery/client/msbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -0,0 +1,49 @@
+package com.brewery.client.msbreweryclient.web.config;
+
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    private ClientHttpRequestFactory clientHttpRequestFactory() {
+
+        /**
+         * Obično su uvijek 3 koraka bez obzira na implementaciju (ovde je bila apache)
+         * setup connectionManagera
+         * setup request configa
+         * setup clienta
+         */
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100); // max total connections
+        connectionManager.setDefaultMaxPerRoute(20);
+
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(3000)
+                .setSocketTimeout(3000)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        restTemplate.setRequestFactory(this.clientHttpRequestFactory());
+    }
+
+}
